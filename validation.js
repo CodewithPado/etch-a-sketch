e1fa1afeb55b266666e8f02dd18f08a458417123
code_with_pado <padraigmullane@gmail.com>
2023-09-30 19:17:51 -0400

    feat: add dynamic event listeners to square_div
    This commit adds event listeners to the square_div element to change its
    background color to a random RGB value on mouseover and reset it to the
    default on mouseout. The randomColorRGB and resetBgColor functions
    handle the color changes.

--- validation.js
+++ validation.js
@@ -9,11 +9,28 @@ function makeInitialGrid(){
     square_div = document.createElement('div');
     square_div.className= 'square';
     container_div.appendChild(square_div);
+    /* Add an event listeners to the div to change and reset its values*/
+    square_div.addEventListener('mouseover', randomColorRGB);
+    square_div.addEventListener('mouseout',resetBgColor);
   }
   return size;
 }
+
+// To generate random color create r,g, & b values separately 
+// Next add an event input since it will now be an event function 
+function randomColorRGB(event){
+  let r= Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+  return event.target.style.backgroundColor =`rgb(${r},${g},${b})`;
+}
+
+// Reset background to default by setting it equal to a blank value
+function resetBgColor(event){
+  return event.target.style.backgroundColor= '';
+}
 makeInitialGrid();
 
-/*Create a variable for the size of the overall grid and in your for loop create the elements so that they are new elements. Instead of having a global div element and appending the same element 256 times*/
+/* Create a variable for the size of the overall grid and in your for loop create the elements so that they are new elements. Instead of having a global div element and appending the same element 256 times*/
 
-// Since flex-box is one dimensional I decided that I need to refactor my code from 2 for loops into one bigger for loop 
+/*Since flex-box is one dimensional I decided that I need to refactor my code from 2 for loops into one bigger for loop */
